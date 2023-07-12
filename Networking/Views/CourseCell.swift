e19ce50a8a32265eda0f3390e07a31846358bff5
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-07-12 15:49:19 +0300

    Initial Commit

---
+++ Networking/Views/CourseCell.swift
@@ -0,0 +1,34 @@
+//
+//  CourseCell.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 21/08/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+final class CourseCell: UITableViewCell {
+
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessons: UILabel!
+    @IBOutlet var numberOfTests: UILabel!
+    
+    private let networkManager = NetworkManager.shared
+    
+    func configure(with course: Course) {
+        courseNameLabel.text = course.name
+        numberOfLessons.text = "Number of lessons: \(course.numberOfLessons ?? 0)"
+        numberOfTests.text = "Number of tests: \(course.numberOfTests ?? 0)"
+        
+        networkManager.fetchImage(from: course.imageUrl!) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.courseImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
